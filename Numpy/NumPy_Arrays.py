0c279bbbf27dec683386c80db8c50adf0f00056e
Abdul Quddous <quddous2369@gmail.com>
2026-07-06 19:19:24 +0500

    Common Array Creation Functions

---
+++ Numpy/NumPy_Arrays.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+# 1D Array 
+# marks = np.array([23,44,56,78])
+# print(marks)
+
+# 2D Array
+# arr = np.array([[23,44,56,78] , [23,56,76,34]])
+# print(arr)
+
+# 3D Array
+# arr = np.array([[23,44,56,78] , [23,56,76,34] , [47,90,15,67]])
+# print(arr)
+
+# Using dtype
+# arr = np.array([1,2,3], dtype=float)
+
+# print(arr)
+
+
+# Common Array Creation Functions
+
+#zeros()
+# arr= np.zeros(5)
+# print(arr)
+
+
+# arr = np.zeros((3,3,3))
+# print(arr)
+
+
+# ones()
+# arr = np.ones((2,2))
+# print(arr)
+
+#full()
+# arr = np.full((2,2),6)
+# print(arr)
+
+#eye()
+# arr = np.eye(3)
+# print(arr)
+
+#arange 
+# arr = np.arange(10,100,5)
+# print(arr)
+
+
+# linespace
+# arr = np.linspace(0,100 , 5 , dtype=int)
+# print(arr)
+
+

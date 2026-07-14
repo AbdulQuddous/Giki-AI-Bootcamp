c59a45aed5ddd6e9e8561cd2c920a6bda57e1991
Abdul Quddous <quddous2369@gmail.com>
2026-07-14 21:02:04 +0500

    Change missing column name in dataset

--- Nlp/text_preprocessing.py
+++ Nlp/text_preprocessing.py
@@ -1,27 +1,42 @@
 
-document = "I love Natural Language Processing."
+# document = "I love Natural Language Processing."
 
-print(document)
+# print(document)
 
-corpus = [
-    "I love NLP.",
-    "Python is amazing.",
-    "NLP makes computers understand language."
-]
+# corpus = [
+#     "I love NLP.",
+#     "Python is amazing.",
+#     "NLP makes computers understand language."
+# ]
 
-print("Corpus Size:", len(corpus))
+# print("Corpus Size:", len(corpus))
 
-print("\nDocuments:\n")
+# print("\nDocuments:\n")
 
-for i, doc in enumerate(corpus, start=1):
-    print(f"Document {i}: {doc}")
+# for i, doc in enumerate(corpus, start=1):
+#     print(f"Document {i}: {doc}")
 
-vocabulary = set()
-for document in corpus:
-    for word in document.split():
-        vocabulary.add(word)
+# vocabulary = set()
+# for document in corpus:
+#     for word in document.split():
+#         vocabulary.add(word)
 
-print("Vocabulary:\n")
-print(vocabulary)
+# print("Vocabulary:\n")
+# print(vocabulary)
 
-print("\nVocabulary Size:", len(vocabulary))
+# print("\nVocabulary Size:", len(vocabulary))
+
+import pandas as pd
+
+df = pd.read_csv('F:/Giki Ai bootcamp/Nlp/reviews.csv')
+
+print(df.head())
+print(df.shape)
+print(df.columns)
+
+list_of_column_names = list(df.columns)
+print('List of column names : ', list_of_column_names)
+df = df.rename(columns={df.columns[2]: 'Text'})
+df.columns
+print(df['Text'][:5])
+
